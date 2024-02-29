7135a7acb17589336b612a3f616a88d5dad66dea
WillyAnb22 <161175216+WillyAnb22@users.noreply.github.com>
2024-02-29 17:29:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -30,36 +30,81 @@ const articulo = [
         img: "./img/pla6.png",
     },
     {
-        name: "Labial",
+        name: "PLANTA",
         precio: 1200,
-        img: "./pla7.png",
+        img: "./img/pla7.png",
     },
     {
-        name: "Labial",
+        name: "PLANTA",
         precio: 1200,
-        img: "./pla8.png",
+        img: "./img/pla8.png",
     },
     {
-        name: "Labial",
+        name: "PLANTA",
         precio: 1200,
-        img: "./pla9.png",
+        img: "./img/pla9.png",
     },
 ];
 
+let data = [];
+
+function enviar(nombre, precio, img) {
+    let datos = {
+        nombre: nombre,
+        precio: precio,
+        img: img
+    };
+    data.push(datos);
+    pintar();
+}
+function pintar() {
+    let tableBody = document.getElementById("listadatos");
+    while (tableBody.firstChild) {
+        tableBody.removeChild(tableBody.firstChild);
+    }
+    data.forEach((item, i) => {
+        let fila = document.createElement("tr");
+
+        let titulo = document.createElement("td");
+        titulo.textContent = item.nombre;
+        fila.appendChild(titulo);
+
+        let precio = document.createElement("td");
+        precio.textContent = item.precio * item.cantidad;
+        fila.appendChild(precio);
+
+        let imagen = document.createElement("td");
+        let img = document.createElement("img");
+        img.src = item.img;
+        imagen.appendChild(img);
+        fila.appendChild(imagen);
+
+
+        let cantidad = document.createElement("td");
+        cantidad.textContent =item.cantidad
+        fila.appendChild(cantidad);
+
+        let boton = document.createElement("button")
+        boton.textContent = "x"
+        fila.appendChild(boton)
+        document.getElementById("listadatos").appendChild(fila);
+    });
+}
+
 let card = document.getElementById("card-template");
-articulo.map((x) => {
+articulo.forEach((x, index) => {
     card.innerHTML += ` 
-    <div class="col">
-    <div class="card h-100">
-      <img src="${x.img}" class="card-img-top" alt="${x.name}">
-      <div class="card-body">
-        <h5 class="card-title text-center text-primary">${x.name}</h5>
-        <p class="card-text">${x.precio}</p>
-      </div>
-      <div class="card-footer">
-        <button href="${x.btn}"class="text-body-secondary">COMPRAR</button>
-      </div>
-    </div>
-  </div>  
-`
-})
+            <div class="col">
+              <div card="cardto">
+                <img src="${x.img}" class="cardfoto" alt="${x.name}">
+                <div class="card-body">
+                  <h5 class="card-title text-center">${x.name}</h5>
+                  <p class="card-text text-center">${x.precio}</p>
+                </div>
+                <div class="enviar">
+                  <button onclick="enviar('${x.name}', ${x.precio}, '${x.img}');" class="btn-buy">COMPRAR</button>
+                </div> 
+              </div>
+            </div>
+            `;
+});
